d7f868198e56a041734a0ca524380d3a73984232
Ruben Dias <rubenmsd@hotmail.com>
2020-04-27 19:16:44 +0100

    landing page and menus

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'menu', to: 'pages#menu'
+  get 'menu-trabalhos', to: 'pages#menu-trabalhos'
+  get 'about', to: 'pages#about'
+
+  # Generic syntax:
+  # verb 'path', to: 'controller#action'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
